9e9c1d37432d4d3d4141e4732d0253faccebe0f8
Sylvain <sylvain.conny@protonmail.com>
2023-02-24 09:24:29 +0100

    all files

---
+++ client.php
@@ -0,0 +1,5 @@
+<?php
+
+require_once __DIR__ . '/twig.php';
+
+$twig->display('client.html.twig', []);
---
+++ contact.php
@@ -0,0 +1,5 @@
+<?php
+
+require_once __DIR__ . '/twig.php';
+
+$twig->display('contact.html.twig', []);
